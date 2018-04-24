4cccabd6bf02b7c847c634493a0f7bd9b2580dd5
Paulus Schoutsen <balloob@gmail.com>
2018-04-24 14:25:24 -0400

    Update sidebars

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -19,17 +19,17 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site' /* title for your website */,
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com' /* your website url */,
+  title: 'Home Assistant dev docs' /* title for your website */,
+  tagline: 'All you need to start developing for Home Assistant',
+  url: 'https://developers.home-assistant.io' /* your website url */,
   baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
+  projectName: 'home-assistant',
+  organizationName: 'home-assistant',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -52,8 +52,8 @@ const siteConfig = {
 
   /* colors for website */
   colors: {
-    primaryColor: '#3fbbf4',
-    secondaryColor: '#205C3B',
+    primaryColor: '#03A9F4',
+    secondaryColor: '#ff9800',
   },
 
   /* custom fonts for website */
@@ -72,7 +72,7 @@ const siteConfig = {
   copyright:
     'Copyright © ' +
     new Date().getFullYear() +
-    ' Your Name or Your Company Name',
+    ' Home Assistant',
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
