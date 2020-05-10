bcaf587a3ce1644499aa2cdacca18792e086dbfa
Franck Nijhof <git@frenck.dev>
2020-05-10 18:46:49 +0200

    Minor site config changes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,80 +1,134 @@
 module.exports = {
-  title: 'Home Assistant Developer Docs',
-  tagline: 'All you need to start developing for Home Assistant',
-  url: 'https://developers.home-assistant.io',
-  baseUrl: '/',
-  favicon: 'img/favicon.png',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "Home Assistant Developer Documentation",
+  tagline: "All you need to start developing for Home Assistant",
+  url: "https://developers.home-assistant.io",
+  baseUrl: "/",
+  favicon: "img/favicon.png",
+  organizationName: "home-assistant",
+  projectName: "developers.home-assistant",
   themeConfig: {
     googleAnalytics: {
-      trackingID: 'UA-57927901-3',
+      trackingID: "UA-57927901-3",
     },
     navbar: {
-      title: 'Developer Docs',
+      title: "Developer Documentation",
       logo: {
-        alt: 'Home Assistant',
-        src: 'img/logo-pretty.svg',
+        alt: "Home Assistant",
+        src: "img/logo-pretty.svg",
       },
       links: [
-        {to: 'docs/architecture_index', label: 'Architecture', position: 'left'},
-        {to: 'docs/frontend_index', label: 'Frontend', position: 'left'},
-        {to: 'docs/development_index', label: 'Backend', position: 'left'},
-        {to: 'docs/misc', label: 'Misc', position: 'left'},
-        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          to: "docs/architecture_index",
+          label: "Architecture",
+          position: "left",
+        },
+        { to: "docs/frontend_index", label: "Frontend", position: "left" },
+        { to: "docs/development_index", label: "Backend", position: "left" },
+        { to: "docs/misc", label: "Misc", position: "left" },
+        { to: "blog", label: "Blog", position: "left" },
       ],
     },
     footer: {
       logo: {
-        alt: 'Home Assistant',
-        src: 'img/logo-white.svg',
-        href: 'https://www.home-assistant.io',
+        alt: "Home Assistant",
+        src: "img/logo-white.svg",
+        href: "https://www.home-assistant.io",
       },
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Social',
+          title: "More Home Assistant",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Homepage",
+              href: "https://www.home-assistant.io",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/home-assistant/developers.home-assistant',
+              label: "Data Science Portal",
+              href: "https://data.home-assistant.io",
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/hass_devs',
+              label: "Alerts",
+              href: "https://alerts.home-assistant.io",
+            },
+            {
+              label: "System Status",
+              href: "https://status.home-assistant.io/",
+            },
+          ],
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              to: "blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/home-assistant",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/hass_devs",
+            },
+            {
+              label: "Discord chat",
+              href: "https://www.home-assistant.io/join-chat",
+            },
+          ],
+        },
+        {
+          title: "Other",
+          items: [
+            {
+              label: "Privacy",
+              href: "https://www.home-assistant.io/privacy/",
+            },
+            {
+              label: "Security",
+              href: "https://www.home-assistant.io/security/",
+            },
+          ],
+        },
+        {
+          items: [
+            {
+              html: `
+              <a href="https://www.netlify.com" target="_blank" rel="noreferrer noopener" aria-label="Deploys by Netlify">
+                <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
+              </a>
+              `,
             },
           ],
         },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Home Assistant, Inc. Built with Docusaurus.`,
     },
-    image: 'img/default-social.png',
+    image: "img/default-social.png",
     algolia: {
-      apiKey: 'd0245452c36b10cec4b8b5bad04e06fc',
-        indexName: 'home-assistant-dev',
-      algoliaOptions: {}
+      apiKey: "d0245452c36b10cec4b8b5bad04e06fc",
+      indexName: "home-assistant-dev",
+      algoliaOptions: {},
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            'https://github.com/home-assistant/developers.home-assistant/edit/master/',
+            "https://github.com/home-assistant/developers.home-assistant/edit/master/",
+          showLastUpdateTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         blog: {
           postsPerPage: 10,
           feedOptions: {
-            type: 'all',
+            type: "all",
           },
         },
       },
