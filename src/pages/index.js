b9b0a6f999e8e1395daf37abf2c51739626429a2
Bartosz Dokurno <bartusekcraft@gmail.com>
2022-07-31 01:36:15 +0200

    Remove unused import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import clsx from 'clsx';
 import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import styles from "./styles.module.css";
