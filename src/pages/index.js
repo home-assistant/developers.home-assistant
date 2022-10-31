77de9f30d99b0e2325303317ed08910d3d362247
ludeeus <ludeeus@ludeeus.dev>
2022-10-31 13:15:23 +0000

    Use TZ from browser in calendar embed

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,7 +98,7 @@ const features = [
       <>
         <iframe
           title="Upcoming Events Calendar"
-          src="https://calendar.google.com/calendar/embed?height=400&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=UTC&amp;src=cDA3bjk4Z28xMW9uYW1kMDhkMGttcTZqaHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23039BE5&amp;title=Release%20Schedule&amp;mode=AGENDA&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=1&amp;showNav=0&amp;showDate=0&amp;showTitle=0"
+          src={`https://calendar.google.com/calendar/embed?height=400&wkst=2&bgcolor=%23ffffff&ctz=${Intl.DateTimeFormat().resolvedOptions().timeZone || "UTC"}&src=cDA3bjk4Z28xMW9uYW1kMDhkMGttcTZqaHNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23039BE5&title=Release%20Schedule&mode=AGENDA&showPrint=0&showTabs=0&showCalendars=0&showTz=1&showNav=0&showDate=0&showTitle=0`}
           style={{ borderWidth: 0, margin: "auto" }}
           width="300"
           height="200"
