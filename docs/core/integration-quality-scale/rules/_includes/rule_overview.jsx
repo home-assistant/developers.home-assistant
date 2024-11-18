0943912611e7375520d52e4d4f7b638838a25a55
Joostlek <joostlek@outlook.com>
2024-11-18 16:23:49 +0100

    Fix missed rules

--- docs/core/integration-quality-scale/rules/_includes/rule_overview.jsx
+++ docs/core/integration-quality-scale/rules/_includes/rule_overview.jsx
@@ -5,10 +5,12 @@ const tiers = require("./tiers.json")
 
 export default function RuleOverview({tier}) {
     const docs = useDocsVersion().docs;
+    console.log(docs)
     return (
         <ul>
             {tiers[tier].map((rule) => {
                 const lowerCaseRule = rule.toLowerCase();
+                console.log(lowerCaseRule)
                 const relatedRule = docs[`core/integration-quality-scale/rules/${lowerCaseRule}`];
                 const [ruleId, ruleText] = relatedRule.title.split(": ");
                 return (
