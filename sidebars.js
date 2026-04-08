fe301e9cf992587741c624ae315e96149b2dc52f
Joost Lekkerkerker <joostlek@outlook.com>
2026-04-08 16:19:14 +0200

    Add subsection for debugging integrations (#3039)

--- sidebars.js
+++ sidebars.js
@@ -142,8 +142,14 @@ module.exports = {
         "creating_integration_manifest",
         "config_entries_config_flow_handler",
         "config_entries_options_flow_handler",
-        "core/integration_diagnostics",
-        "core/integration_system_health",
+        {
+          type: "category",
+          label: "Debugging an integration",
+          items: [
+            "core/integration/diagnostics",
+            "core/integration/system_health",
+          ],
+        },
         "configuration_yaml_index",
         "dev_101_services",
         "creating_platform_index",
